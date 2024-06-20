b15b7210460281dd89938e3a09bee931f7faab9a
student <student@SlovakstudyUbuntu.myguest.virtualbox.org>
2024-06-20 18:16:36 +0200

    Третій коміт хз що робить

--- k.c
+++ k.c
@@ -37,16 +37,97 @@
 //     }
 //     return false;
 // }
-bool is_move_possible(const struct game game)
+// bool is_move_possible(const struct game game)
+// {
+//     for (int i = 0; i < SIZE-1; i++)
+//     {
+//         for (int j = 0; j < SIZE-1; j++)
+//         {
+//         if(game.board[i][j]==game.board[i+1][j]||game.board[i][j]==game.board[i][j+1]||game.board[i+1][j]==' '||game.board[i][j+1]==' '){
+//             return true;
+//         }
+//         }
+//     }
+//     return false;
+// }
+bool update(struct game *game, int dy, int dx)
 {
-    for (int i = 0; i < SIZE-1; i++)
+    if (dy > 1 || dy < -1|| dx > 1 || dx < -1)
     {
-        for (int j = 0; j < SIZE-1; j++)
-        {
-        if(game.board[i][j]==game.board[i+1][j]||game.board[i][j]==game.board[i][j+1]){
-            return true;
-        }
-        }
+        return false;
     }
-    return false;
+    if ((dy != 0 && dx != 0) || (dy == 0 && dx == 0))
+    {
+        return false;
+    }
+    if (dx = 1)
+    {
+        
+        for(int j=0;j<SIZE;j++)
+        {
+            for(int i=SIZE-1;i!=0;i--)
+            {
+                while(game->board[i][j]){
+                    
+                }
+            }
+        }
+        
+
+        
+    }
+    if (dx = -1)
+    {
+    
+    }
+    if (dy = 1)
+    {
+    
+    }
+    if (dy = -1)
+    {
+    
+    }
 }
+// if (dy != 0)
+//     {
+//         if (dy == 1)
+//         {
+//             for (int i = 0; i < SIZE; i++)
+//             {
+//                 for (int j = 0; j < SIZE; j++)
+//                 {
+//                 }
+//             }
+//         }
+//         if (dy == -1)
+//         {
+//             for (int i = 0; i < SIZE; i++)
+//             {
+//                 for (int j = 0; j < SIZE; j++)
+//                 {
+//                 }
+//             }
+//         }
+//     }
+//     if (dx != 0)
+//     {
+//         if (dx == 1)
+//         {
+//             for (int i = 0; i < SIZE; i++)
+//             {
+//                 for (int j = 0; j < SIZE; j++)
+//                 {
+//                 }
+//             }
+//         }
+//         if (dx == -1)
+//         {
+//             for (int i = 0; i < SIZE; i++)
+//             {
+//                 for (int j = 0; j < SIZE; j++)
+//                 {
+//                 }
+//             }
+//         }
+//     }
